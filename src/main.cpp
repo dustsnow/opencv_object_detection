b8fc65ef5c09b5655f797d988a23c4ef948146e6
Peng Hou <penghou620@gmail.com>
2013-10-10 22:52:46 -0400

    Trying using coutour; doesn't work well

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,49 @@ int UpperH = 180;
 int UpperS = 256;
 int UpperV = 256;
 int HC_Param1 = 200;
-int HC_Param2 = 60;
-int HL_Threshold = 100;
+int HC_Param2 = 30;
+int HL_Threshold = 30;
+int HL_MinLineLength = 1;
 Mat img_hsv,img_mask;
+Mat frame;
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
 
-void thresh_callback(int,void*);
+Mat drawing;
+vector<vector<Point> > contours;
+void thresh_callback(int,void*){
+	Mat canny_output;
+    vector<Vec4i> hierarchy;
+    /// Detect edges using canny
+    Canny( img_mask, canny_output, thresh, thresh*2, 3 );
+    /// Find contours
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+	vector<vector<Point> > contours_poly( contours.size() );
+	for(int i = 0; i < contours.size(); i++){
+		approxPolyDP( Mat(contours[i]), contours_poly[i],3,false);
+	}
+
+    /// Draw contours
+    drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+	   #if 0
+       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+	   #else
+       drawContours( drawing, contours_poly, i, color, 2, 8, hierarchy, 0, Point() );
+	   #endif
+    }
+
+    /// Show in a window
+    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    imshow( "Contours", drawing );
+    //vector<int> compression_params;
+    //compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+    //compression_params.push_back(9);
+	//imwrite("contours.jpg",drawing,compression_params);
+}
 void createTrackbars(){
 	namedWindow("Control");
 	createTrackbar("LowerH","Control",&LowerH,180,NULL);
@@ -29,6 +67,7 @@ void createTrackbars(){
 	createTrackbar("HC_Param1","Control",&HC_Param1,500,NULL);
 	createTrackbar("HC_Param2","Control",&HC_Param2,450,NULL);
 	createTrackbar("HL_Threshold","Control",&HL_Threshold,450,NULL);
+	createTrackbar("HL_MinLineLength","Control",&HL_MinLineLength,400,NULL);
 
 }
 void colorDetection(Mat &frame){
@@ -46,9 +85,8 @@ int main(int argc, char** argv){
 		return -1;
 	}
 
-	Mat frame;
 	while(1){
-		#if 1
+		#if 0
 		cap >> frame;
 		#else 
 		frame = imread(argv[1],1);	
@@ -61,17 +99,35 @@ int main(int argc, char** argv){
 		colorDetection(frame);
 		imshow("Img Mask", img_mask);
 
+		//#if 1
+		//Mat frame_gray;
+		//cvtColor(frame,frame_gray,CV_BGR2GRAY);
+		//blur(frame_gray,frame_gray,Size(3,3));
+		blur(img_mask,img_mask,Size(3,3));
+		namedWindow("Source",CV_WINDOW_AUTOSIZE);
+		imshow("Source", frame);
+		createTrackbar("Canny thresh:","Source",&thresh, max_thresh, thresh_callback);
+		thresh_callback(0,0);
+
+		//waitKey(10);
+		//#else
 		//Circle Detection
+			
+		//cvtColor(frame,img_mask,CV_BGR2GRAY); //Using original image to detect circle and line
+		img_mask = drawing.clone();//using color-filtered image to detect circle and line
+		cvtColor(img_mask,img_mask,CV_BGR2GRAY);
 		GaussianBlur(img_mask, img_mask, Size(9, 9), 2, 2);
 		vector<Vec3f> circles;
 		HoughCircles(img_mask, circles, CV_HOUGH_GRADIENT, 1, img_mask.rows/8, HC_Param1, HC_Param2, 0, 0);
 
+
 		//Line Detection
-		
 	    Mat dst, cdst;
 		Canny(img_mask,dst,10,200,3);
 	    vector<Vec2f> lines;
 	    HoughLines(dst, lines, 1, CV_PI/180, HL_Threshold, 0, 0 );
+	    //vector<Vec4i> lines;
+	    //HoughLinesP(dst, lines, 1, CV_PI/180, HL_Threshold, HL_MinLineLength, 40 );
 	    for( size_t i = 0; i < lines.size(); i++ )
 	    {
 	        float rho = lines[i][0], theta = lines[i][1];
@@ -96,7 +152,7 @@ int main(int argc, char** argv){
     	}
 
 		imshow("Detected", frame);
-
+		//#endif
 		if(waitKey(10)>=0) break;
 	}
 	return 0;
